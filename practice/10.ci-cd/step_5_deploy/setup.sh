44662c43f5e9cce3165114ea2e909e337ea22345
Сергей Бондарев <s.bondarev@southbridge.ru>
2020-03-20 15:01:47 +0300

    Update setup.sh

--- practice/10.ci-cd/step_5_deploy/setup.sh
+++ practice/10.ci-cd/step_5_deploy/setup.sh
@@ -47,7 +47,7 @@ then
         metadata:
           name: $CI_PROJECT_PATH_SLUG-$CI_ENVIRONMENT_NAME
         rules:
-        - apiGroups: ["", "extensions", "apps", "certmanager.k8s.io"]
+        - apiGroups: ["", "extensions", "apps", "certmanager.k8s.io", "cert-manager.io"]
           resources: ["*"]
           verbs: ["*"]
 EOF
